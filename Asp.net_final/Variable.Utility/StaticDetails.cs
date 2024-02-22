1e430447ab900c4db9915cc0437390a58a2ebd8f
Yogesh patel <yogeshtech79@gmail.com>
2024-02-22 19:54:34 +0530

    add

---
+++ Asp.net_final/Variable.Utility/StaticDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable.Utility
+{
+    public static class StaticDetails
+    {
+        public const String Role_Customer = "Customer";
+        public const String Role_Company = "Company";
+        public const String Role_Admin = "Admin";
+        public const String Role_Employee = "Employee";
+    }
+}
